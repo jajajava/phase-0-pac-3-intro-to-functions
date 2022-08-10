f97d4d0bc484d3d35b538aa077828518c570b1f9
David <davidstepanov@yahoo.com>
2022-08-09 21:17:27 -0400

    commit tree 8922e76b70ae059e35a06ea1e3c37c024ff7499e

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Follow along with the examples here
+function sayHello(){
+    console.log("HELLO")
+}
+
+sayHello()
+
+function sayHelloGuadalupe(){
+    console.log("Hello Guadalupe")
+}
+
+function sayHelloLiz(){
+    console.log("Hello Liz")
+}
+
+function sayHelloSamip(){
+    console.log("Hello Samip")
+}
+
+sayHelloGuadalupe(), sayHelloLiz(), sayHelloSamip()
+
+function add(x,y){
+    return x + y
+}
+    console.log(add(2,3))
+
+    function say(greeting, firstName) {
+        console.log("I was called!");
+        return `${greeting}, ${firstName}!`;
+      }
+console.log("Hello","David")
+
+
